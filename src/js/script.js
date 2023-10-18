56dafa2ee86a48e9458df6c12d948f7aa6bb4bf1
tardio717 <tardiojaime@users.noreply.github.com>
2023-10-18 19:35:26 -0400

    version terminada para mobiles y desktop

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,40 @@
 const contenedor = document.getElementById("detalles");
-
-// Obtén el elemento
+var region = document.getElementById("region");
+var region2 = document.getElementById("region2");
 var elemento = document.getElementById("detalles");
-
+var heigth = window.innerWidth;
+var media = 992;
+var bottom_sticky = document.getElementById("sticky_bottom");
+//media>heigth ?
+console.log("El ancho de la pantalla es: " + heigth + " píxeles");
+var departamento = document.getElementById("departamento");
+var departamento2 = document.getElementById("departamento2");
 // Obtén la posición del elemento
 var btn_enlace = document.getElementById("btn_ocultar");
-
+region.addEventListener("change", function (e) {
+  this.value != "Bolivia"
+    ? departamento.classList.add("d-none")
+    : departamento.classList.remove("d-none");
+  console.log("seleccionado " + this.value);
+});
+region2.addEventListener("change", function (e) {
+  this.value != "Bolivia"
+    ? departamento2.classList.add("d-none")
+    : departamento2.classList.remove("d-none");
+  console.log("seleccionado " + this.value);
+});
 window.addEventListener("scroll", function () {
-  var posicion = elemento.getBoundingClientRect();
-  if (posicion.top === 0) {
+  const posicion = elemento.getBoundingClientRect();
+  heigth = window.innerWidth;
+  if (posicion.top < 0) {
     btn_enlace.classList.remove("d-none");
   } else {
-    console.log("El elemento no ción." + posicion.top);
     btn_enlace.classList.add("d-none");
     // Comprueba si el elemento está en la parte superior de la ventana de visualización
+  }
+  if (heigth < media && posicion.top < 0) {
+    bottom_sticky.style.display = "block";
+  } else {
+    bottom_sticky.style.display = "none";
   }
 });
