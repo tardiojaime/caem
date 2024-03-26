666667d652d921d8f67fa70ed9d2e520050a259a
tardiojaime <tardiojaime@users.noreply.github.com>
2024-03-26 16:08:45 -0400

    cambios realizados en desing dia martes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 const path = require("path");
 
 export default {
+  base: './',
   root: path.resolve(__dirname, "src"),
   build: {
     outDir: "../docs",
     assetsDir: "assets", // Ruta relativa a outDir
+    rollupOptions: {
+      input:{
+        main: 'src/index.html',
+        invitacion: 'src/invitacion.html'
+      }
+    }
   },
   server: {
     port: 8080,
